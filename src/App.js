15946a4655c950ed97163ed5da693aaa8bffd29a
Jose Maria Zamora Perez <jzamora@biada.net>
2021-06-15 18:35:37 +0200

    estilo de sections y de la pagina

--- src/App.js
+++ src/App.js
@@ -1,55 +1,150 @@
-
-
 function App() {
   return (
     <>
-    <main>
-      <section>
-    <form>
-      <label htmlFor="nombre"> Nombre</label>
-      <input type="text" id="nombre"/>
-      <label htmlFor="apellido">Apellidos</label>
-      <input type="text" id="apellido" />
-      <label htmlFor="fecha">Fecha de nacimiento</label>
-      <input  type="date" id="fecha"/>
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email"/>
-      <button type="submit">Generar datos</button>
-    </form>
-    </section>
-    <section>
-    <form>
-    <label htmlFor="nombrelogin">Nick para logear</label>
-      <input type="text" id="nombrelogin" />
-    <label htmlFor="password">Contraseña</label>
-      <input type="password"  id="password"/>
-      <label htmlFor="repeat-password">Repetir contraseña</label>
-      <input type="password" id="repeat-password"/>
-      <button type="submit">Registrarse</button>
-    </form>
-    </section>
-    <section>
-    <form>
-     <label htmlFor="nombrelogin">Nick de login</label>
-      <input type="text" id="nombrelogin"/>
-      <label htmlFor="password">Contraseña</label>
-      <input type="password" id="password" />
-      <label htmlFor="remember-password">Recordar contraseña?</label>
-      <input type="checkbox" id="remember-password" />
-      <button type="submit">Logearse</button>
-    </form>
-    </section>
-    <section>
-      <ul>
-        <li>nombre</li>
-        <li>apellidos</li>
-        <li>email</li>
-        <li>nick name</li>
-        <li>fecha de nacimiento</li>
-        <li>contraseña</li>
-      </ul>
-    </section>
-    </main>
+      <header className="d-flex align-items-center justify-content-center p-5 bg-primary text-white border-bottom">
+        <h1>Crea una cuenta/logeate</h1>
+      </header>
+      <main className="p-5 vw-100">
+        <section className="container formulario-seccion shadow-sm  mb-5 bg-white rounded ">
+          <form className="row p-3 justify-content-center">
+            {/* <label className="col-sm-6 col-form-label" htmlFor="nombre">
+              Nombre
+            </label> */}
+            <div className="input-group col-sm-12 mb-4">
+              <div className="input-group-prepend ">
+                <label className="input-group-text" htmlFor="nombre">
+                  Nombre
+                </label>
+              </div>
+              <input className="form-control" type="text" id="nombre" />
+            </div>
+
+            <div className="input-group col-sm-12 mb-4">
+              <div className="input-group-prepend ">
+                <label className="input-group-text" htmlFor="apellido">
+                  Apellidos
+                </label>
+              </div>
+              <input className="form-control" type="text" id="apellido" />
+            </div>
+            <div className="input-group col-sm-12 mb-4">
+              <div className="input-group-prepend ">
+                <label className="input-group-text" htmlFor="fecha">
+                  Nacimiento
+                </label>
+              </div>
+              <input className="form-control" type="date" id="fecha" />
+            </div>
+            <div className="input-group col-sm-12 mb-4">
+              <div className="input-group-prepend ">
+                <label className="input-group-text" htmlFor="email">
+                  Email
+                </label>
+              </div>
+              <input className="form-control" type="email" id="email" />
+            </div>
+            <button type="submit" className="btn btn-primary mt-3 col-sm-8">
+              Siguiente
+            </button>
+          </form>
+        </section>
+        <section className="container formulario-seccion shadow-sm  mb-5 bg-white rounded ">
+          <form className="row p-3 justify-content-center">
+            <div className="input-group col-sm-12 mb-4">
+              <div className="input-group-prepend ">
+                <label className="input-group-text" htmlFor="apodo">
+                  Apodo
+                </label>
+              </div>
+              <input className="form-control" type="text" id="apodo" />
+            </div>
+
+            <div className="input-group col-sm-12 mb-4">
+              <div className="input-group-prepend ">
+                <label className="input-group-text" htmlFor="passwordRegistro">
+                  Contraseña
+                </label>
+              </div>
+              <input
+                className="form-control"
+                type="password"
+                id="passwordRegistro"
+              />
+            </div>
+            <div className="input-group col-sm-12 mb-4">
+              <div className="input-group-prepend ">
+                <label className="input-group-text" htmlFor="confirm">
+                  Confirmar
+                </label>
+              </div>
+              <input className="form-control" type="password" id="confirm" />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary mt-3 mr-3 col-sm-4"
+            >
+              Atras
+            </button>
+            <button type="submit" className="btn btn-primary mt-3 col-sm-4">
+              Registrar
+            </button>
+          </form>
+        </section>
+
+        <section className="container formulario-seccion shadow-sm  mb-5 bg-white rounded ">
+          <form className="row p-3 justify-content-center">
+            <div className="input-group col-sm-12 mb-4">
+              <div className="input-group-prepend ">
+                <label className="input-group-text" htmlFor="apodoLog">
+                  Apodo
+                </label>
+              </div>
+              <input className="form-control" type="text" id="apodoLog" />
+            </div>
+            <div className="input-group col-sm-12 mb-4">
+              <div className="input-group-prepend ">
+                <label className="input-group-text" htmlFor="passwordLog">
+                  Contraseña
+                </label>
+              </div>
+              <input
+                className="form-control"
+                type="password"
+                id="passwordLog"
+              />
+            </div>
+            <div className="form-check col-sm-12 text-right">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="recordar"
+              />
+              <label className="form-check-label recordar" htmlFor="recordar">
+                Recordar Contraseña?
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary mt-3 mr-3 col-sm-4"
+            >
+              Atras
+            </button>
+            <button type="submit" className="btn btn-primary mt-3 col-sm-4">
+              Iniciar Sessión
+            </button>
+          </form>
+        </section>
+        <section className="container formulario-seccion  p-3 mb-5 bg-white rounded d-flex justify-content-center">
+          <ul className="list-group w-75 shadow-sm ">
+            <li className="list-group-item">nombre:</li>
+            <li className="list-group-item">apellidos:</li>
+            <li className="list-group-item">email:</li>
+            <li className="list-group-item">nick name:</li>
+            <li className="list-group-item">fecha de nacimiento:</li>
+            <li className="list-group-item">contraseña:</li>
+          </ul>
+        </section>
+      </main>
     </>
   );
 }
